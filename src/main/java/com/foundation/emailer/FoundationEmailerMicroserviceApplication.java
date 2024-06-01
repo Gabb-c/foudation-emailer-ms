e25726c32d7674195ebf529fb7708ea2bf2e4891
Gabb-c <gdgabrieldacunha71@gmail.com>
2024-06-01 17:31:29 -0300

    initial commit

---
+++ src/main/java/com/foundation/emailer/FoundationEmailerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.foundation.emailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationEmailerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoundationEmailerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foundation/emailer/FoundationEmailerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundation.emailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundationEmailerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
